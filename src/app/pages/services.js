1c84f02c56509e122e93174841ba39ea5bdff6a6
francoluca35 <francolucap1@gmail.com>
2024-06-27 23:52:30 -0300

    primer actualizacion de la pagina
    se realizo el navbar de la pagina.

---
+++ src/app/pages/services.js
@@ -0,0 +1,10 @@
+// src/pages/services.js
+export default function Services() {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Services</h1>
+        <p>Details of the services we offer.</p>
+      </div>
+    );
+  }
+  
