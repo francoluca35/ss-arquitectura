c934eb664e97a9c4cbacc476bace07859469cfb7
francoluca35 <francolucap1@gmail.com>
2024-07-08 01:31:43 -0300

    Tercer actualizacion
    -se realizo el home, navbar, responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,15 @@
 import React from "react";
-import Image from "next/image";
-import Logo from "/public/assets/logo-1.png";
+import Carousel from "@/components/home/Carousel";
+import Estudiohome from "@/components/home/Estudio-home";
 function page() {
   return (
     <>
-
-      <div className="relative h-screen w-screen overflow-hidden">
-        <video
-          autoPlay
-          loop
-          muted
-          src="/videos/home.mp4"
-          className="absolute top-0 left-0 w-full h-full object-cover"
-        ></video>
-           <div className="relative flex flex-col justify-center items-center h-full bg-black bg-opacity-50 text-center text-white">
-          <Image src={Logo} alt="Logo" className="w-[400px] h-30 mb-2" /> 
-          <p className="text-xl max-w-xl">
-            donde la creatividad y la innovación se fusionan para dar forma a
-            entornos inspiradores. Somos un equipo apasionado de arquitectos
-            comprometidos con la excelencia en el diseño y la construcción.
-          </p>
-        </div>
-      </div>
+      <Carousel />
+      <Estudiohome />
     </>
   );
 }
 
 export default page;
+
+
